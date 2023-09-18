5c111c9944f563ba38f2be38a9ce5ed95e2387a7
liter-alb-dev <liter.alb.dev@gmail.com>
2023-09-18 07:38:14 +0300

    created routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+
+    //no-login required routes
+    Route::post("users/login", [UserController::class, "login"]);
+    Route::post("users/signup", [UserController::class, "signup"]);
+
+    //login required routes
+    Route::middleware('auth:sanctum')->group(function () {
+        //Users routes
+        Route::delete("users/logout", [UserController::class, "logout"]);
+        Route::apiResource("users", UserController::class)
+            ->except(["index", "store"]);
+
+        //Shipping adresses routes
+        Route::apiResource("addresses", Shipping_AddressesController::class)
+            ->except(["show, index"]);
+
+        //Whishlist routes
+        Route::apiResource("whishlist", WhishlistController::class)
+            ->except(["show", "update"]);
+
+        //Categories routes
+        Route::apiResource("categories", CategoryController::class)
+            ->only(["index", "show"]);
+
+        //Products routes
+        Route::apiResource("products", ProductController::class)
+            ->only(["index", "show"]);
+
+        //Products' reviews routes
+        Route::apiResource("products/reviews", Product_ReviewsController::class)
+            ->except(["show"]);
+
+        //Orders routes
+        Route::apiResource("orders", OrderController::class)
+            ->except(["show", "update"]);
+    });
 });
